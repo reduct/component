69999e423619a19c7b1111d8e5149a3fa5733061
Tyll Weiß <inkdpixels@googlemail.com>
2015-06-27 22:36:35 +0200

    [TASK] Add a basic Element API test regarding the getElement() method #8

---
+++ Tests/API/Element.js
@@ -0,0 +1,16 @@
+var nodeProto = require('./../../Src/NodeProto.js');
+
+// An ExampleClass for testing purposes.
+class ExampleComponent extends nodeProto.Component {
+	constructor(el, props) {
+		super(el, props)
+	}
+}
+
+describe('NodeProto: Element API', function () {
+    it('The element should be defined', function () {
+    	var componentInstance = new ExampleComponent();
+
+        expect(componentInstance.getElement()).toBeDefined();
+    });
+});
