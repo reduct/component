ceccb3828049c48ef648bf7db58948758301be5f
Tyll Weiß <weiss@sitegeist.de>
2015-06-27 15:27:28 +0200

    [TASK] Cleanup

--- Build/Jest.ES6.Layer.js
+++
@@ -1,9 +0,0 @@
-var nodeProto = require('./../Src/NodeProto.js');
-
-class ExampleComponent extends nodeProto.Component {
-    constructor(element, props) {
-        super(element, props)
-    }
-}
-
-module.exports = ExampleComponent;
